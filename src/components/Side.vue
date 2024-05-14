0df8cce5559089bc8afd5d6515acfdf5c741cf4a
Dylen_Light <1610854495@qq.com>
2024-05-15 03:54:55 +0800

    增加了请求ap信息的功能

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -1,25 +1,23 @@
 <template>
-    <v-navigation-drawer>
+    <v-navigation-drawer style="width: 20%;">
         <v-list>
             <v-list-item title="Accessible APs"></v-list-item>
-            <v-expansion-panels v-model="panel" multiple  variant="popout">
-                <v-expansion-panel v-for="info in AP_info" >
+            <v-expansion-panels v-model="panel" multiple variant="popout">
+                <v-expansion-panel v-for="info in AP_info">
                     <v-expansion-panel-title>
                         AP : {{ info.apid }}
                     </v-expansion-panel-title>
                     <v-expansion-panel-text>
-                        <v-card :subtitle=info.isValid>
-
+                        <v-card>
                             <v-card-title>
                                 Position:[{{ info.pos_x }},{{ info.pos_y }}]
                             </v-card-title>
-
                             <v-card-subtitle>
                                 isValid:{{ info.isValid }}
                             </v-card-subtitle>
                             <v-card-text>
-                                current distance: {{ info.distance }}
-                                <br>isLoS: {{info.isLoS }}
+                                current distance: {{ info.dist }}
+                                <br>isLoS: {{ info.isLoS }}
                             </v-card-text>
                         </v-card>
                     </v-expansion-panel-text>
@@ -30,69 +28,56 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     created() {
         
     },
+    mounted() {
+        setInterval(() => {
+            axios.get('http://localhost:5000/ap')
+                .then(response => {
+                    //console.log(response.data);
+                    // const array = Object.keys(response.data).map(key => ({
+                    //     id: key,
+                    //     ...response.data[key]
+                    // }));
+                    // console.log(array)
+                    const _this = this;
+                    this.AP_info = [];
+                    for (let i = 0; i < response.data.length; i++) {
+                        let current_data = response.data[i];
+                        console.log(current_data);
+                        this.AP_info.push(
+                            {
+                                apid: current_data.apid,
+                                pos_x: current_data.pos_x,
+                                pos_y: current_data.pos_y,
+                                isValid: Boolean(current_data.is_valid),
+                                dist: String(current_data.dist),
+                                isLoS: Boolean(current_data.is_los),
+                            }
+                        )
+                    }
+                    console.log(_this.AP_info);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }, 1000 * 5);
+    },
     data() {
         return {
             panel: [],
-            AP_info: [
-                {
-                    apid:0,
-                    pos_x:0,
-                    pos_y:0,
-                    isValid: true,
-                    distance: 0,
-                    isLoS: true
-                }, {
-                    apid:1,
-                    pos_x:1,
-                    pos_y:1,
-                    isValid: false,
-                    distance: 0,
-                    isLoS: false
-                }
-            ]
+            AP_info: []
         }
+    },
+    methods: {
+        
     },
 }
 
-// const AP_info = reactive([{
-//     bssid: "ftm_00000000",
-//     isValid: true,
-//     distance: 0,
-//     isLoS: true
-// }],
-//     [{
-//         bssid: "ftm_00000001",
-//         isValid: false,
-//         distance: 0,
-//         isLoS: false
-//     }]);
-// export default{
-//   name:"Index",
-//   data() {
-//     return {
-//         AP_info: [{
-//                 apid:-1,
-//                 bssid: "ftm_00000000",
-//                 isValid: true,
-//                 distance: 0,
-//                 isLoS: true
-//             }]
-
-//         }
-//   },
-//   created(){
-//   },
-//   methods:{
-//     // login(){
-//     //   const _this = this;
-//     //   _this.$router.push("/login");
-//     // }
-//   }
-// }
 
 
 </script>
