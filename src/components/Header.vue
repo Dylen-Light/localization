37e520dcf2b72089bd8acb5bbed77a99e18bb657
Dylen_Light <1610854495@qq.com>
2024-05-16 02:41:22 +0800

    添加了定位显示

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <template>
-    <v-layout class="rounded rounded-md" style="height: 100%;">
+    <v-layout class="rounded rounded-md" style="height: 100%;" color = "primary">
         <v-app-bar title="Indoor Localization Based on FTM"></v-app-bar>
-        <Side />
         <Main />
     </v-layout>
 </template>
@@ -9,7 +8,6 @@
   
 <script setup>
 import Main from '@/components/MainContent.vue';
-import Side from '@/components/Side.vue';
     //
 </script>
   
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,19 +1,148 @@
 <template>
+    <v-navigation-drawer style="width: 20%;">
+        <v-list>
+            <v-list-item title="Accessible APs"></v-list-item>
+            <v-expansion-panels v-model="panel" multiple variant="popout">
+                <v-expansion-panel v-for="info in AP_info">
+                    <v-expansion-panel-title>
+                        AP : {{ info.apid }}
+                    </v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <v-card>
+                            <v-card-title>
+                                Position:[{{ info.pos_x }},{{ info.pos_y }}]
+                            </v-card-title>
+                            <v-card-text>
+                                current distance: {{ info.dist }}
+                                <br>isLoS: {{ info.isLoS }}
+                                <br>isValid:{{ info.isValid }}
+                            </v-card-text>
+                        </v-card>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </v-list>
+    </v-navigation-drawer>
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;height: 100%;">
-        <v-card class="mx-auto" prepend-icon="$vuetify" subtitle="The #1 Vue UI Library" width="400">
-            <template v-slot:title>
-                <span class="font-weight-black">Welcome to Vuetify</span>
-            </template>
-
-            <v-card-text class="bg-surface-light pt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore
-                voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum
-                unde voluptatem!
-            </v-card-text>
-        </v-card>
+        <div ref="myChart" id="myChart" style="height: 100%; width: 100%;">
+        </div>
     </v-main>
 </template>
 
-<script setup>
-    //
+<script>
+import axios from 'axios';
+import * as echarts from 'echarts';
+
+export default {
+    created() {
+
+    },
+    mounted() {
+        setInterval(() => {
+            const dom = this.$refs['myChart']; // 获取dom节点
+            const myChart = echarts.init(dom); // 初始化echarts实例
+            // 设置实例参数
+            myChart.setOption(this.option);
+            const _this = this;
+            _this.AP_info = [];
+            _this.option.series = [];
+            axios.get('http://localhost:5000/ap')
+                .then(response => {
+
+                    for (let i = 0; i < response.data.length; i++) {
+                        let current_data = response.data[i];
+                        console.log(current_data);
+                        _this.AP_info.push(
+                            {
+                                apid: current_data.apid,
+                                pos_x: current_data.pos_x,
+                                pos_y: current_data.pos_y,
+                                isValid: Boolean(current_data.is_valid),
+                                dist: current_data.dist.toFixed(2) + " cm",
+                                isLoS: Boolean(current_data.is_los),
+                            }
+                        );
+                        _this.option.series.push(
+                            {
+                                type: 'scatter',
+                                symbolSize: current_data.dist*2,
+                                data: [
+                                    [current_data.pos_x, current_data.pos_y]
+                                ]
+                            },
+                            {
+                                type: 'effectScatter',
+                                symbolSize: 20,
+                                data: [
+                                    [current_data.pos_x, current_data.pos_y]
+                                ]
+                            }
+                        )
+                    }
+                    //console.log(_this.AP_info);
+
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+            axios.get('http://localhost:5000/res')
+                .then(response => {
+                    console.log(response.data);
+                    _this.option.series.push(
+                        {
+                            type: 'scatter',
+                            symbolSize: 20,
+                            data: [
+                                [response.data[0], response.data[1]]
+                            ]
+                        }
+                    )
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            
+        }, 1000 * 5);
+    },
+    data() {
+        return {
+            panel: [],
+            AP_info: [],
+            option: {
+                xAxis: {
+                    scale: true
+                },
+                yAxis: {
+                    scale: true
+                },
+                series: [
+                    // {
+                    //     type: 'scatter',
+                    //     symbolSize: 200,
+                    //     data: [
+                    //         [172.7, 115.2],
+                    //         [163.4, 42]
+                    //     ]
+                    // },
+                    // {
+                    //     type: 'effectScatter',
+                    //     symbolSize: 20,
+                    //     data: [
+                    //         [172.7, 115.2],
+                    //         [163.4, 42]
+                    //     ]
+                    // },
+                ]
+            }
+        }
+    },
+    methods: {
+
+    },
+}
+
+
+
 </script>
